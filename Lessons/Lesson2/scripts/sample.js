4b0cedf9c536b98b4bb3b17cd9642edef1087c53
turbo25mike <turbo25mike@gmail.com>
2018-02-09 10:35:18 -0800

    more to lesson2

--- Lessons/Lesson2/scripts/sample.js
+++ Lessons/Lesson2/scripts/sample.js
@@ -1,38 +1,40 @@
-var breakout = (function () {
-    var canvas = document.getElementById("myCanvas");
-    var ctx = canvas.getContext("2d");
-    var ballRadius = 10;
-    var x = canvas.width / 2;
-    var y = canvas.height - 30;
-    var dx = 2;
-    var dy = -2;
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
 
-    function drawBall() {
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
     }
 
-    function draw() {
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        drawBall();
+    x += dx;
+    y += dy;
+}
 
-        if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
-            dx = -dx;
-        }
-        if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
-            dy = -dy;
-        }
 
-        x += dx;
-        y += dy;
-    }
+//step 1
+var titleObj = document.getElementById("title");
+titleObj.innerText = "Welcome To";
 
-    return {
-        draw
-    };
-})();
 
-setInterval(breakout.draw, 10);
+//step 2
+setInterval(draw, 10);
