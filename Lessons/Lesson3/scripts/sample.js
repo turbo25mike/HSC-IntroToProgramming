93d0acc3b80aa033b462794a518f1b56c784c250
turbo25mike <turbo25mike@gmail.com>
2018-02-15 21:18:40 -0800

    added lesson3

---
+++ Lessons/Lesson3/scripts/sample.js
@@ -0,0 +1,42 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
+var dx = 2;
+var dy = -2;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
+
+
+function draw() {
+    document.getElementById("pdx").innerText = "x: "+x;
+    document.getElementById("pdy").innerText = "y: "+y;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+
+    //set next position
+    if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+        dx = -dx;
+    }
+    if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
+
+    x += dx;
+    y += dy;
+}
+
+
+//step 1
+var titleObj = document.getElementById("title");
+titleObj.innerText = "Welcome To";
+
+//step 2
+setInterval(draw, 10);
